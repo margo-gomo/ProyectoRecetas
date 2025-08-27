328f58ce450975b53c22c4b0874c5f376ba64db7
Danyjosu69 <josue.acuna.bolanos@est.una.ac.cr>
2025-08-27 10:53:42 -0600

    Update Paciente.java

--- src/main/java/entidades/Paciente.java
+++ src/main/java/entidades/Paciente.java
@@ -1,1 +1,21 @@
+package entidades;
+import lombok.*;
+import java.time.LocalDate;
 
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Paciente {
+    @Getter
+    @Setter
+    private String nombre;
+    @Getter
+    @Setter
+    private int id;
+    @Getter
+    @Setter
+    private int telefono;
+    @Getter
+    @Setter
+    private LocalDate fecha_nacimiento;
+}
