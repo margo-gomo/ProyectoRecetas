f29463701b386c02da1b4c2b51076588a75d5528
Maki <maria.gonzalez.montenegro@est.una.ac.cr>
2025-09-06 19:38:30 -0600

    Nuevo login e interfaz de cambiar contraseña

---
+++ src/main/java/Vista/CambiarClaveVista.java
@@ -0,0 +1,17 @@
+package Vista;
+
+import javax.swing.*;
+
+public class CambiarClaveVista {
+    private JPanel panel1;
+    private JTextField idText;
+    private JLabel idLabel;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JPasswordField passwordField3;
+    private JLabel ContraActualField;
+    private JLabel nuevaContraField;
+    private JLabel confirmarField;
+    private JButton aceptarButton;
+    private JButton cancelarButton;
+}
