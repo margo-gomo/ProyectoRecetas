2e6f87358086020d231545816e3a9646190400a9
Danyjosu69 <josue.acuna.bolanos@est.una.ac.cr>
2025-09-08 21:26:53 -0600

    farmaceuta y despacho

---
+++ src/main/java/Controlador/Usuarios/ControladorUsuarioFarmaceuta.java
@@ -0,0 +1,79 @@
+package Controlador.Usuarios;
+import Despacho.DespachoReceta;
+import Modelo.ModeloRecetas;
+import Prescripcion.PrescripcionReceta;
+import jakarta.xml.bind.JAXBException;
+
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
+
+public class ControladorUsuarioFarmaceuta extends DespachoReceta {
+    public ControladorUsuarioFarmaceuta(ModeloRecetas modelo) {
+        this.modelo = modelo;
+    }
+    public ControladorUsuarioFarmaceuta() {
+        this(new ModeloRecetas());
+    }
+    public void init() {
+        try {
+            modelo.cargar();
+        } catch (JAXBException | FileNotFoundException ex) {
+            System.err.printf("Ocurrió un error al cargar los datos: '%s'%n",
+                    ex.getMessage());
+        }
+    }
+    public PrescripcionReceta buscarReceta(int idPaciente, LocalDate fechaConfeccion) {
+        return modelo.buscarReceta(idPaciente, fechaConfeccion);
+    }
+
+    public PrescripcionReceta agregar(PrescripcionReceta receta) throws IllegalArgumentException {
+        return  modelo.agregar(receta);
+    }
+
+    public PrescripcionReceta actualizar(PrescripcionReceta recetaPorActualizar) throws IllegalArgumentException {
+        return modelo.actualizar(recetaPorActualizar);
+    }
+
+    public PrescripcionReceta eliminar(int idPaciente, LocalDate fechaConfeccion) throws IllegalArgumentException {
+        return modelo.eliminar(idPaciente, fechaConfeccion);
+    }
+    public void cerrarAplicacion() {
+        try {
+            modelo.guardar();
+        } catch (JAXBException | FileNotFoundException ex) {
+            System.err.printf("Ocurrió un error al guardar los datos: '%s'%n",
+                    ex.getMessage());
+        }
+
+        System.out.println("Aplicación finalizada..");
+        System.exit(0);
+    }
+    public boolean iniciarProceso(int idPaciente, LocalDate fechaRetiro) throws IllegalArgumentException {
+        for(PrescripcionReceta receta: modelo.obtenerListaRecetas()){
+            if(receta.getPaciente().getId() == idPaciente && receta.getFecha_retiro().equals(fechaRetiro)){
+                actualizar(iniciarProceso(receta));
+                return true;
+            }
+        }
+        return  false;
+    }
+    public boolean marcarLista(int idPaciente, LocalDate fechaRetiro)throws IllegalArgumentException{
+        for(PrescripcionReceta receta: modelo.obtenerListaRecetas()){
+            if(receta.getPaciente().getId() == idPaciente && receta.getFecha_retiro().equals(fechaRetiro)){
+                actualizar(marcarLista(receta));
+                return true;
+            }
+        }
+        return  false;
+    }
+    public boolean entregar(int idPaciente, LocalDate fechaRetiro)throws IllegalArgumentException{
+        for(PrescripcionReceta receta: modelo.obtenerListaRecetas()){
+            if(receta.getPaciente().getId() == idPaciente && receta.getFecha_retiro().equals(fechaRetiro)){
+                actualizar(entregar(receta));
+                return true;
+            }
+        }
+        return  false;
+    }
+    private ModeloRecetas modelo;
+}
