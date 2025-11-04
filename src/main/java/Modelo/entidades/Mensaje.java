96a64e01a9a8133e3d07dbc69a19c3cdd369e7f0
Danyjosu69 <josue.acuna.bolanos@est.una.ac.cr>
2025-11-03 18:03:27 -0600

    cambios para mensaje

--- src/main/java/Modelo/entidades/Mensaje.java
+++ src/main/java/Modelo/entidades/Mensaje.java
@@ -2,44 +2,26 @@ package Modelo.entidades;
 
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-
-import java.util.Date;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @DatabaseTable(tableName = "mensaje")
 public class Mensaje {
-    @DatabaseField(generatedId = true)
-    private Long id;
+    @DatabaseField(id = true)
+    private String id;
 
-    @DatabaseField
-    private String remitente;
+    @DatabaseField(foreign = true, foreignAutoRefresh = true,canBeNull = false)
+    private Usuario remitente;
 
-    @DatabaseField
-    private String destinatario;
+    @DatabaseField(foreign = true, foreignAutoRefresh = true,canBeNull = false)
+    private Usuario destinatario;
 
-    @DatabaseField
+    @DatabaseField(canBeNull = false)
     private String texto;
 
-    @DatabaseField(columnName = "fecha_envio")
-    private Date fechaEnvio;
-
-    public Mensaje() {}
-
-    public Mensaje(String remitente, String destinatario, String texto, Date fechaEnvio) {
-        this.remitente = remitente;
-        this.destinatario = destinatario;
-        this.texto = texto;
-        this.fechaEnvio = fechaEnvio;
-    }
-
-    public Long getId() { return id; }
-    public String getRemitente() { return remitente; }
-    public String getDestinatario() { return destinatario; }
-    public String getTexto() { return texto; }
-    public Date getFechaEnvio() { return fechaEnvio; }
-
-    public void setId(Long id) { this.id = id; }
-    public void setRemitente(String r) { this.remitente = r; }
-    public void setDestinatario(String d) { this.destinatario = d; }
-    public void setTexto(String t) { this.texto = t; }
-    public void setFechaEnvio(Date f) { this.fechaEnvio = f; }
+    @DatabaseField(canBeNull = false)
+    private int leido;
 }
